ba56ac4e0091fe96b625992d89aa2b91215fad6e
hhutku <hhutku.wa@gmail.com>
2020-10-31 00:39:17 -0700

    comments added

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+// finding  and asigning the elements to variables
 var header = document.querySelector(".header");
 var highestScore = document.querySelector(".highScores");
 var instruction = document.querySelector(".instruction");
@@ -6,31 +7,27 @@ var questionChoice = document.querySelector(".question-choice");
 var question = document.querySelector(".question");
 var choices = document.querySelectorAll(".choices");
 var displayTime = document.querySelector(".displayTime");
-var body=document.querySelector("body");
-var listener=document.querySelector(".listener");
-var containerHighScores=document.querySelector(".containerHighScores");
+var body = document.querySelector("body");
+var listener = document.querySelector(".listener");
+var containerHighScores = document.querySelector(".containerHighScores");
 
-if(localStorage.getItem("highestScore")){
-  highestScore.textContent="High Score : " +localStorage.getItem("highestScore");}
 
-var nextQ = 0;
+//assigning variables
+var questionNumber = 0;
 var timer;
-var count = 100;
+var countDown = 60;
 var score;
 var submitButton;
 var input;
 var allDone;
-var  submitingScore;
+var resultTable;
 var storedScores;
-
 var clearHighScoresbuton;
 var scoresTable;
-var point=0;
+var point = 0;
+var isCorrect;
 
-
-
-
-
+// questions array
 var questions = [
     {
         question: "Which one is not a data type? ",
@@ -82,282 +79,210 @@ var questions = [
         answers: ["round", "rnd", "Math.round", "floor"],
         correctAnswer: "Math.round"
     }];
-
+// function for quiz timing  
 var startQuiz = function () {
-
-       timer = setInterval(function () {
-
-        count--;
-
-        displayTime.textContent = count;
-        if( count<=0){
+    timer = setInterval(function () {
+        countDown--;
+        displayTime.textContent = countDown;
+        // When countdown 0 or under zero game over
+        if (countDown <= 0) {
             clearTimeout(timer);
-            score=point;
-            questionChoice.setAttribute("style","display:none");
+            score = point;
+            questionChoice.setAttribute("style", "display:none");
             resultShow();
         }
 
     }, 1000);
-
+    //hiding first page elements
     instruction.setAttribute("style", "display:none");
     startButton.setAttribute("style", "display:none");
+    // showing the question block and choices block
     question.setAttribute("style", "display:block");
-
     for (i = 0; i < choices.length; i++) {
         choices[i].setAttribute("style", "display:block");
     }
-
+    // Displaying first question and answer
     for (i = 0; i < choices.length; i++) {
-
-        choices[i].textContent = questions[nextQ].answers[i];
+        choices[i].textContent = questions[questionNumber].answers[i];
     }
-
-    question.textContent = questions[nextQ].question;
-
-    nextQ++;
-
+    question.textContent = questions[questionNumber].question;
+    //getting  the next question
+    questionNumber++;
 };
-
-
-
-
-var isCorrect;
+//selecting answer function
 var chooseAnswer = function (e) {
-
-  
-
-    if( (e.target.innerText!="Wrong !" && e.target.innerText!="Correct !" && e.target.className!="question"))
-    // if( (e.target.dataset!="isCorrect" && e.target.className!="question"))
-    // if(e.target.dataset)
-    {
-
-    
-
-    if (nextQ <= questions.length) {
+// determining the correct place for selecting answer     
+    if ((e.target.innerText != "Wrong !" && e.target.innerText != "Correct !" && e.target.className != "question")) {
+// displaying the next question 
+        if (questionNumber <= questions.length) {
 
             if (isCorrect) {
-           
-            questionChoice.removeChild(isCorrect);
-        }
-
-        if (e.target.outerText == questions[nextQ - 1].correctAnswer) {
-
-            isCorrect = document.createElement('div');
-            questionChoice.appendChild(isCorrect);
-            isCorrect.setAttribute("style", "border-top: 2px solid rgb(158, 62, 62);color:rgb(158, 62, 62);margin-top:0px;padding:10px;font-size:20px");
-            isCorrect.textContent = "Correct !";
-            isCorrect.setAttribute("data-set","isCorrect");
-           point=point+10;
-
-        } else {
-            isCorrect = document.createElement('div');
-            questionChoice.appendChild(isCorrect);
-            isCorrect.setAttribute("style", "border-top: 2px solid rgb(158, 62, 62);color:rgb(158, 62, 62);margin-top:0px;padding:10px;font-size:20px");
-            isCorrect.setAttribute("data-set","isCorrect");
-            isCorrect.textContent = "Wrong !";
-            count -= 10;
-            if (count < 0) {
-                count = 1;
-
+// removing old answers result                
+                questionChoice.removeChild(isCorrect);     
+            }
+//checking the answer
+            if (e.target.outerText == questions[questionNumber - 1].correctAnswer) {
+//if answer is correct display "correct" under the choices
+                isCorrect = document.createElement('div');
+                questionChoice.appendChild(isCorrect);
+                isCorrect.setAttribute("style", "border-top: 2px solid rgb(158, 62, 62);color:rgb(158, 62, 62);margin-top:0px;padding:10px;font-size:20px");
+                isCorrect.textContent = "Correct !";
+                isCorrect.setAttribute("data-set", "isCorrect");
+                point = point + 10;
+//if answer is wrong display "wrong" under the choices and substract 10 seconds
+            } else {
+                isCorrect = document.createElement('div');
+                questionChoice.appendChild(isCorrect);
+                isCorrect.setAttribute("style", "border-top: 2px solid rgb(158, 62, 62);color:rgb(158, 62, 62);margin-top:0px;padding:10px;font-size:20px");
+                isCorrect.setAttribute("data-set", "isCorrect");
+                isCorrect.textContent = "Wrong !";
+                countDown -= 10;
+//arrange the displayed time to "0" in case it takes minus values
+                if (countDown < 0) {
+                    countDown = 1;
+                }
             }
         }
+// finalize the game if the questions end
+        if (questionNumber < questions.length && e.target.className == "choices") {
+            for (i = 0; i < choices.length; i++) {
+                choices[i].textContent = questions[questionNumber].answers[i];
+            }
+            question.textContent = questions[questionNumber].question;
+        }
+       
 
+//Waiting for 1 sec to display the last questions result and Display the RESULTTABLE
+        if (questions.length == questionNumber) {
+            setTimeout(callbackFunction, 1000);
+            function callbackFunction() {
+                clearTimeout(timer);
+                score = point;
+                questionChoice.setAttribute("style", "display:none");
+                resultShow();
+            }
+        }
+        questionNumber++;
     }
+}
 
-    if (nextQ < questions.length && e.target.className == "choices") {
-
-        for (i = 0; i < choices.length; i++) {
-
-            choices[i].textContent = questions[nextQ].answers[i];
+//Creating and Displaying the result Table function
+resultShow = function () {
+    // hiding the question and answers part
+            questionChoice.setAttribute("style", "display:none")
+          //creating and displaying result table and setting attributes
+            resultTable = document.createElement('div');
+            document.body.appendChild(resultTable);
+            allDone = document.createElement('h2')
+            resultTable.appendChild(allDone);
+            allDone.textContent = "All done !";
+            allDone.setAttribute("style", "text-align: center; color:rgb(158, 62, 62);margin-top:100px;font-size:40px;");
+            var finalScore = document.createElement('p');
+            allDone.insertAdjacentElement('afterend', finalScore);
+            finalScore.textContent = "Your finalscore is " + score + "."
+            finalScore.setAttribute("style", "text-align: center; color:rgb(158, 62, 62);margin-top:50px;font-size:30px;")
+            var form = document.createElement('form');
+            finalScore.insertAdjacentElement('afterend', form);
+            form.setAttribute("style", "text-align: center;color:rgb(158, 62, 62);margin-top:50px;font-size:20px");
+            var label = document.createElement('label');
+            form.appendChild(label);
+            label.setAttribute("for", "fname");
+            label.textContent = "Enter initials :"
+            input = document.createElement('input');
+            form.appendChild(input);
+            input.setAttribute("type", "text");
+            input.setAttribute("id", "scoreText")
+            submitButton = document.createElement('input');
+            form.appendChild(submitButton);
+            submitButton.setAttribute("type", "submit");
+            submitButton.setAttribute("value", "submit")
+            submitButton.setAttribute("value", "submit")
         }
 
-        question.textContent = questions[nextQ].question;
-
-
-    }
-
-      resultShow=function(){
-
-        questionChoice.setAttribute("style","display:none")
-
-       submitingScore=document.createElement('div');
-      document.body.appendChild(submitingScore);
-
-       allDone= document.createElement('h2')
-            
-       submitingScore.appendChild(allDone);
-        allDone.textContent="All done !";
-        allDone.setAttribute("style","text-align: center; color:rgb(158, 62, 62);margin-top:100px;font-size:40px;");
-
-        var finalScore=document.createElement('p');
-        allDone.insertAdjacentElement('afterend', finalScore);
-        finalScore.textContent="Your finalscore is "+ score +"."
-        finalScore.setAttribute("style","text-align: center; color:rgb(158, 62, 62);margin-top:50px;font-size:30px;")
-
-        var form=document.createElement('form');
-        finalScore.insertAdjacentElement('afterend', form);
-        form.setAttribute("style","text-align: center;color:rgb(158, 62, 62);margin-top:50px;font-size:20px");
-
-        var label=document.createElement('label');
-        form.appendChild(label);
-        label.setAttribute("for","fname");
-        label.textContent="Enter initials :"
-        input=document.createElement('input');
-        form.appendChild(input);
-        input.setAttribute("type","text");
-        input.setAttribute("id","scoreText")
-     submitButton=document.createElement('input');
-        form.appendChild(submitButton);
-        submitButton.setAttribute("type","submit");
-        submitButton.setAttribute("value","submit")
-         submitButton.setAttribute("value","submit")
-      
-        
-    }
-       
-
-    if ( questions.length == nextQ ) {
-
-        setTimeout( callbackFunction, 1500 );
-        function callbackFunction(){
-        
-        clearTimeout(timer);
-        
-        score=point;
-        questionChoice.setAttribute("style","display:none");
-        resultShow(); }
-    }
-    nextQ++;
-
+// function for creating and displaying  goback and clearscore button
+function displayGobackAndClearScore() {
+    goBack = document.createElement('input');
+    clearHighScoresbtn = document.createElement('input');
+    listener.appendChild(clearHighScoresbtn);
+    listener.appendChild(goBack);
+    goBack.setAttribute("type", "submit");
+    clearHighScoresbtn.setAttribute("type", "submit");
+    clearHighScoresbtn.setAttribute("class", "clear");
+    goBack.setAttribute("value", "GoBack");
+    goBack.setAttribute("class", "goBack");
+    goBack.setAttribute("style", "margin-left:50px;margin-top:50px;color:rgb(56, 10, 10); font-size:20px; cursor: pointer;background:rgb(158, 62, 62)")
+    clearHighScoresbtn.setAttribute("style", "margin-left:30px;color:rgb(56, 10, 10); font-size:20px;margin-top:50px; cursor: pointer;background:rgb(158, 62, 62)")
+    clearHighScoresbtn.setAttribute("value", "ClearHighScores");
+    goback = document.querySelector(".clear");
+    clearHighScoresbuton = document.querySelector(".clear");
+    goBack = document.querySelector(".goBack");
+    // Restarting the game
+    goBack.addEventListener("click", function (e) {
+        e.preventDefault();
+        window.location.href = "https://hhutku.github.io/code-quiz/";
+    });
+    // Clearing the result scores
+    clearHighScoresbuton.addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.clear();
+        containerHighScores.setAttribute("style", "display:none");
+    });
 }
 
-}
-
-
-startButton.addEventListener("click", startQuiz);
-var choice = questionChoice.addEventListener("click", chooseAnswer);
-
-
-
-function displayGobackAndClearScore(){
-
-    goBack=document.createElement('input');
-     clearHighScoresbtn=document.createElement('input');
-    listener.appendChild( clearHighScoresbtn);
-    listener.appendChild( goBack);
-    goBack.setAttribute("type","submit");
-    clearHighScoresbtn.setAttribute("type","submit");
-    clearHighScoresbtn.setAttribute("class","clear");
-    goBack.setAttribute("value","GoBack");
-    goBack.setAttribute("class","goBack");
-    goBack.setAttribute("style","margin-left:50px;margin-top:50px;color:rgb(56, 10, 10); font-size:20px; cursor: pointer;background:rgb(158, 62, 62)")
-    clearHighScoresbtn.setAttribute("style","margin-left:30px;color:rgb(56, 10, 10); font-size:20px;margin-top:50px; cursor: pointer;background:rgb(158, 62, 62)")
-    clearHighScoresbtn.setAttribute("value","ClearHighScores");
-    goback=document.querySelector(".clear");
-   clearHighScoresbuton=document.querySelector(".clear");
-   goBack=document.querySelector(".goBack");
-
-   goBack.addEventListener("click", function(e){
-    e.preventDefault();
-       
-    
-       window.location.href = "https://hhutku.github.io/code-quiz/";
- 
-
-
-   });
-
-
-   clearHighScoresbuton.addEventListener("click", function(e){
-    e.preventDefault();
-    
-
-         localStorage.clear();
-       
-          
-        containerHighScores.setAttribute("style","display:none");
-       
-       
-
-   });
-
-
-}
-
-
-function scoreDisplay(){
-    
-
-    submitingScore.setAttribute("style","display:none");
-    
-    
-
-    var list=document.createElement('ul');
+// Display High scores function 
+function scoreDisplay() {
+    // displaying go back and clear score buttons
+    displayGobackAndClearScore();
+// hiding result show page
+    resultTable.setAttribute("style", "display:none");
+// creating high score lines
+    var list = document.createElement('ul');
     containerHighScores.appendChild(list);
     list.append("High Scores");
-    list.setAttribute("style","margin-left:450px;color:rgb(158, 62, 62);font-size:40px;margin-bottom:40px");
+    list.setAttribute("style", "margin-left:450px;color:rgb(158, 62, 62);font-size:40px;margin-bottom:40px");
 
+    for (i = 0; i < storedScores.length; i++) {
+        if (i < 10) {
+            console.log(storedScores[i].name);
+            console.log(storedScores[i].score);
+            var li = document.createElement('li');
+            list.insertAdjacentElement('beforeend', li);
+            li.setAttribute("style", " color:rgb(158, 62, 62);margin-top:10px;font-size:20px;margin-left:40px")
+            li.textContent = storedScores[i].name + " :" + storedScores[i].score;
+            localStorage.setItem("highestScore", storedScores[0].score);
+        }
+    }
+}
 
+// Starting the quiz
+startButton.addEventListener("click", startQuiz);
 
-     displayGobackAndClearScore();
-       
-      
- 
-    for(i=0;i<storedScores.length;i++){
-if(i<5){
-    
-    console.log(storedScores[i].name);
-    console.log(storedScores[i].score);
-        var li=document.createElement('li');
-        list.insertAdjacentElement('beforeend', li);
-        li.setAttribute("style"," color:rgb(158, 62, 62);margin-top:10px;font-size:20px;margin-left:40px")
-         li.textContent=storedScores[i].name+" :"+ storedScores[i].score;
-        
-         localStorage.setItem("highestScore",storedScores[0].score);
+//Selecting the choices
+var choice = questionChoice.addEventListener("click", chooseAnswer);
 
-        
-         
+// submiting the initials(name) and score and storing the scores in localstorage
+body.addEventListener("submit", function (e) {
+    e.preventDefault();
+      if (e.target.nodeName == "FORM") {
+        var scoresTable = [{ name: input.value, score: score }];
+        storedScores = JSON.parse(localStorage.getItem("highScores"));
+        if (storedScores !== null) {
+            storedScores.push(scoresTable[0]);
+        } else {
+            storedScores = scoresTable;
+        }
+        localStorage.setItem("highScores", JSON.stringify(storedScores));
+        storedScores = JSON.parse(localStorage.getItem("highScores"));
+        storedScores.sort((a, b) => (a.score < b.score) ? 1 : -1);
+        scoreDisplay();
     }
 
-}
-}
-
-body.addEventListener("submit",function(e){
-    e.preventDefault();
-    console.log("eventtttt");
-console.log(e);
-
-    if(e.target.nodeName=="FORM"){
-   
-    var scoresTable=[{name:input.value,score:score}];
-     storedScores=JSON.parse(localStorage.getItem("highScores")); 
-    
-    if (storedScores !== null) { 
-        storedScores.push(scoresTable[0]); 
-      } else {
-        storedScores = scoresTable;
-      }
-
-      localStorage.setItem("highScores", JSON.stringify(storedScores));
-
-    storedScores = JSON.parse(localStorage.getItem("highScores")); 
-    storedScores.sort((a,b) => (a.score < b.score) ? 1: -1);
-
-    
-    
-    scoreDisplay();}
-    
 })
 
-
-
-
-
-
-
-
-
-
+//Displaying highscore
+if (localStorage.getItem("highestScore")) {
+    highestScore.textContent = "High Score : " + localStorage.getItem("highestScore");
+}
 
 
 
